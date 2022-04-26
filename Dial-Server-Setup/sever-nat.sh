311601b4a9784a2f0c1cb7a6c0e882bfd91e175d
Duong Huynh Bao <duonghuynhbao@Duongs-MacBook-Pro.local>
2022-04-26 23:18:15 +0700

    Initial commit

--- Dial-Server-Setup/sever-nat.sh
+++ Dial-Server-Setup/sever-nat.sh
@@ -14,7 +14,8 @@ sysctl net.ipv4.ip_forward
 
 
 
-ethernet_src_iface=enp0s3
+
+ethernet_src_iface=eth1
 ethernet_share_iface=ppp0
 sudo iptables --flush
 sudo iptables --table nat --flush
@@ -22,16 +23,15 @@ sudo iptables --delete-chain
 sudo iptables --table nat --delete-chain
 
 
-sudo iptables -t nat -A POSTROUTING -o $ethernet_src_iface -j MASQUERADE
+# sudo iptables -t nat -A POSTROUTING -o $ethernet_src_iface -j MASQUERADE
 
-sudo iptables -A FORWARD -i $ethernet_share_iface -o $ethernet_src_iface -m state --state RELATED,ESTABLISHED -j>
-sudo iptables -A FORWARD -i $ethernet_src_iface -o $ethernet_share_iface -j ACCEPT
-
-#view
-sudo iptables -L -n -v
-sudo iptables -t nat -L -n -v
+# sudo iptables -A FORWARD -i $ethernet_share_iface -o $ethernet_src_iface -m state --state RELATED,ESTABLISHED -j>
+# sudo iptables -A FORWARD -i $ethernet_src_iface -o $ethernet_share_iface -j ACCEPT
+# #view
+# sudo iptables -L -n -v
+# sudo iptables -t nat -L -n -v
 
 # or
 
-sudo iptables -t nat -A POSTROUTING -s 10.9.0.0/24 -o ens5 -j MASQUERADE
-
+sudo iptables -t nat -A POSTROUTING -s 10.9.0.0/24 -o $ethernet_src_iface -j MASQUERADE
+sudo iptables -t nat -L -n -v
